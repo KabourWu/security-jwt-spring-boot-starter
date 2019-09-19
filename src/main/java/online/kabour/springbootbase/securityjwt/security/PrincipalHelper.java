a28a414200da954dd4c87c67e978162709509da5
kabour <1192852795@qq.com>
2019-09-19 13:54:27 +0800

    @other

--- src/main/java/online/kabour/springbootbase/securityjwt/security/PrincipalHelper.java
+++ src/main/java/online/kabour/springbootbase/securityjwt/security/PrincipalHelper.java
@@ -17,70 +17,70 @@ import java.util.Objects;
  */
 public class PrincipalHelper {
 
-	/**
-	 * 获取登录会员的基本信息
-	 *
-	 * @return
-	 */
-	public static UserContext.UserBasicDetail userBasicDetail() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication instanceof AnonymousAuthenticationToken) {
-			return null;
-		}
-		JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
-		UserContext userContext = (UserContext) jwtAuthenticationToken.getPrincipal();
-		return userContext.getUserBasicDetail();
-	}
+    /**
+     * 获取登录会员的基本信息
+     *
+     * @return
+     */
+    public static UserContext.UserBasicDetail userBasicDetail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
+        UserContext userContext = (UserContext) jwtAuthenticationToken.getPrincipal();
+        return userContext.getUserBasicDetail();
+    }
 
-	/**
-	 * 检查是否是当事人
-	 *
-	 * @param principalIdentity
-	 * @return
-	 */
-	public static boolean principalCheck(String principalIdentity) {
-		return Objects.equals(principalIdentity, principalIdentity());
-	}
+    /**
+     * 检查是否是当事人
+     *
+     * @param principalIdentity
+     * @return
+     */
+    public static boolean principalCheck(String principalIdentity) {
+        return Objects.equals(principalIdentity, principalIdentity());
+    }
 
 
-	/**
-	 * 获取登录用户的 唯一标识
-	 *
-	 * @return
-	 */
-	public static String principalIdentity() {
-		return userBasicDetail() == null ? null : userBasicDetail().getIdentity();
-	}
+    /**
+     * 获取登录用户的 唯一标识
+     *
+     * @return
+     */
+    public static String principalIdentity() {
+        return userBasicDetail() == null ? null : userBasicDetail().getIdentity();
+    }
 
-	/**
-	 * 获取登录用户的用户名
-	 *
-	 * @return
-	 */
-	public static String principalUsername() {
-		return userBasicDetail() == null ? null : userBasicDetail().getUsername();
-	}
+    /**
+     * 获取登录用户的用户名
+     *
+     * @return
+     */
+    public static String principalUsername() {
+        return userBasicDetail() == null ? null : userBasicDetail().getUsername();
+    }
 
-	/**
-	 * 获取登录用户的头像
-	 *
-	 * @return
-	 */
-	public static String principalIcon() {
-		return userBasicDetail() == null ? null : userBasicDetail().getIcon();
-	}
+    /**
+     * 获取登录用户的头像
+     *
+     * @return
+     */
+    public static String principalIcon() {
+        return userBasicDetail() == null ? null : userBasicDetail().getIcon();
+    }
 
-	/**
-	 * 获取登录用户的类型
-	 *
-	 * @return
-	 */
-	public static LoginRequest.UserType principalType() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication instanceof JwtAuthenticationToken) {
-			UserContext userContext = (UserContext) authentication.getPrincipal();
-			return userContext.getUserType();
-		}
-		return LoginRequest.UserType.ANONYMOUS;
-	}
+    /**
+     * 获取登录用户的类型
+     *
+     * @return
+     */
+    public static LoginRequest.UserType principalType() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken) {
+            UserContext userContext = (UserContext) authentication.getPrincipal();
+            return userContext.getUserType();
+        }
+        return LoginRequest.UserType.ANONYMOUS;
+    }
 }
