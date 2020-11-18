0e3b826d4fc202489b13f84ff3413183da6ab53f
kabour <1192852795@qq.com>
2020-11-18 14:52:22 +0800

    @task 修改系统密码加密方式可用户自定义

--- src/main/java/online/kabour/springbootbase/securityjwt/security/auth/ajax/AjaxAuthenticationProvider.java
+++ src/main/java/online/kabour/springbootbase/securityjwt/security/auth/ajax/AjaxAuthenticationProvider.java
@@ -10,6 +10,7 @@ import online.kabour.springbootbase.securityjwt.security.model.UserContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -28,6 +29,7 @@ import java.util.concurrent.TimeUnit;
 public class AjaxAuthenticationProvider implements AuthenticationProvider {
     private static final Logger LOGGER = LoggerFactory.getLogger(AjaxAuthenticationProvider.class);
 
+    @Lazy
     @Autowired
     private PasswordEncoder encoder;
 
